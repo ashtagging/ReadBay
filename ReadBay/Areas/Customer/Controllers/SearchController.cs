8ceb04dd55030571d58722107a10962d01cafc40
liam9061 <60947617+Liam9061@users.noreply.github.com>
2021-04-08 12:30:51 +0100

    Added a simple search function under the area Customer.

---
+++ ReadBay/Areas/Customer/Controllers/SearchController.cs
@@ -0,0 +1,187 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ReadBay.DataAccess.Data.Repository.IRepository;
+using ReadBay.Models;
+using ReadBay.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReadBay.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class SearchController : Controller
+    {
+        // Need to get from dependency injection in startup.cs
+        private readonly IUnitOfWork _unitOfWork;
+
+        // Need this to be able to upload images to the server into a folder
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public SearchController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        // Index View GET: Admin/Product
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        //GET: Admin/Product/Upsert
+        public IActionResult Upsert(int? id)
+        {
+            // View Model added to hold product object and incorporate drop downs for Category & Book Type
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                BookTypeList = _unitOfWork.BookType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+            if (id == null)
+            {
+                //this is for create
+                return View(productVM);
+            }
+            //this is for edit
+            productVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            if (productVM.Product == null)
+            {
+                return NotFound();
+            }
+            return View(productVM);
+
+        }
+
+        // Upsert Post Action Method
+        // Incorporates
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+
+                // if files.Count> 0 File is uploaded
+                if (files.Count > 0)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"Images\Products");
+                    var extenstion = Path.GetExtension(files[0].FileName);
+
+                    if (productVM.Product.ImageUrl != null)
+                    {
+                        //If this is true then it is an edit and we need to remove old image
+
+                        var imagePath = Path.Combine(webRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+                    }
+                    // Upload new file after old file is deleted
+                    using (var filesStreams = new FileStream(Path.Combine(uploads, fileName + extenstion), FileMode.Create))
+                    {
+                        files[0].CopyTo(filesStreams);
+                    }
+                    productVM.Product.ImageUrl = @"\Images\Products\" + fileName + extenstion;
+                }
+                else
+                {
+                    //Update when the image is not altered
+                    if (productVM.Product.Id != 0)
+                    {
+                        Product objFromDb = _unitOfWork.Product.Get(productVM.Product.Id);
+                        productVM.Product.ImageUrl = objFromDb.ImageUrl;
+                    }
+                }
+
+
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            // Load ProductVM.CategoryList
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                });
+                productVM.BookTypeList = _unitOfWork.BookType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                });
+                if (productVM.Product.Id != 0)
+                {
+                    productVM.Product = _unitOfWork.Product.Get(productVM.Product.Id);
+                }
+            }
+            return View(productVM);
+        }
+
+        
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            //Retrieve all the Products and return in a Json format (dataTables)
+            // Include properties added so the category or Book Type can be displayed in the UI
+            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category,BookType");
+            return Json(new { data = allObj });
+        }
+
+        // API Call for Delete called in Product.js
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Product.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            //Deletes the Image from the image folder
+            string webRootPath = _hostEnvironment.WebRootPath;
+            var imagePath = Path.Combine(webRootPath, objFromDb.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(imagePath))
+            {
+                System.IO.File.Delete(imagePath);
+            }
+            // If the object is not null we will delete
+            _unitOfWork.Product.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+
+        #endregion
+    }
+}
+
