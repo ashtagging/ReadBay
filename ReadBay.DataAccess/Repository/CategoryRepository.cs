a9bd80a0e89c42ab7ee05f4c47cedbbb17178f5b
ASHLEY LITTLEWOOD <ashley.littlewood22@gmail.com>
2022-09-18 13:55:09 +0100

    Added Book Photos

--- ReadBay.DataAccess/Repository/CategoryRepository.cs
+++ ReadBay.DataAccess/Repository/CategoryRepository.cs
@@ -14,6 +14,7 @@ namespace ReadBay.DataAccess.Repository
         
         private readonly ApplicationDbContext _db;
 
+        // Dependency Injection
         public CategoryRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
